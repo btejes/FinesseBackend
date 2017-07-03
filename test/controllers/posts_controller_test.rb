04a25d163fdef85fd0b7c6de1e5a8e11a949f921
btejes <btejes@gmail.com>
2017-07-03 09:14:37 -0700

    First commit

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+require 'json'
+
+class PostsControllerTest < ActionController::TestCase
+
+  test "Should get valid list of posts" do
+    get :index, params: { page: { number: 2 } }
+    assert_response :success
+    jdata = JSON.parse response.body
+    assert_equal Post.per_page, jdata['data'].length
+    assert_equal jdata['data'][0]['type'], 'posts'
+    l = jdata['links']
+    assert_equal l['first'], l['prev']
+    assert_equal l['last'], l['next']
+    assert_equal Post.count, jdata['meta']['total-count']
+    assert_equal 'CC-0', jdata['meta']['licence']
+  end
+
+  test "Should get properly sorted list" do
+    post = Post.order('rating DESC').first
+    get :index, params: { sort: '-rating' }
+    assert_response :success
+    jdata = JSON.parse response.body
+    assert_equal post.title, jdata['data'][0]['attributes']['title']
+  end
+
+  test "Should get filtered list" do
+    get :index, params: { filter: 'First' }
+    assert_response :success
+    jdata = JSON.parse response.body
+    assert_equal Post.where(category: 'First').count, jdata['data'].length
+  end
+
+end
